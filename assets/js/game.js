a6c8a39249506fffa0af580fa5c7d619c6beb89a
Dan Reeves <heydanreeves@gmail.com>
2014-05-01 22:30:32 +0100

    added some more sprites for walking animation and figured out scaling to look left/right

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,22 +3,22 @@
 
     function preload () {
         game.load.image('ground', 'assets/img/ground.gif');
-        game.load.image('man', 'assets/img/man.gif');
+        game.load.spritesheet('man', 'assets/img/man.gif', 26, 62, 4);
     }
 
     function create () {
-        // Define movement constants
-        this.MAX_SPEED = 250; // pixels/second
-        this.ACCELERATION = 600; // pixels/second/second
-        this.DRAG = 800; // pixels/second
-        this.GRAVITY = 980; // pixels/second/second
-        this.JUMP_SPEED = -400; // pixels/second (negative y is up)
+
+        this.MAX_SPEED = 250;
+        this.ACCELERATION = 600;
+        this.DRAG = 800;
+        this.GRAVITY = 980;
+        this.JUMP_SPEED = -400;
 
         this.game.stage.backgroundColor = 0x4488cc;
 
         this.ground = this.game.add.group();
         for(var x = 0; x < this.game.width; x += 32) {
-            // Add the ground blocks, enable physics on each, make them immovable
+
             var groundBlock = this.game.add.sprite(x, this.game.height - 32, 'ground');
             this.game.physics.enable(groundBlock, Phaser.Physics.ARCADE);
             groundBlock.body.immovable = true;
@@ -27,22 +27,25 @@
         }
 
         this.player = this.game.add.sprite(this.game.width/2, this.game.height - 100, 'man');
+        this.player.anchor.setTo(0.5, 1);
+        this.player.animations.add('walk',[1,2,3]);
+
         this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
         this.player.body.collideWorldBounds = true;
 
-        // Enable physics on the player
+
         this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
 
-        // Make player collide with world boundaries so he doesn't leave the stage
+
         this.player.body.collideWorldBounds = true;
 
-        // Set player minimum and maximum movement speed
-        this.player.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED * 10); // x, y
 
-        // Add drag to the player that slows them down when they are not accelerating
-        this.player.body.drag.setTo(this.DRAG, 0); // x, y
+        this.player.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED * 10);
 
-        // Since we're jumping we need gravity
+
+        this.player.body.drag.setTo(this.DRAG, 0);
+
+
         game.physics.arcade.gravity.y = this.GRAVITY;
 
         this.game.input.keyboard.addKeyCapture([
@@ -55,24 +58,27 @@
     }
 
     function update () {
-        // Collide the player with the ground
+
         this.game.physics.arcade.collide(this.player, this.ground);
 
         if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-            // If the LEFT key is down, set the player velocity to move left
             this.player.body.acceleration.x = -this.ACCELERATION;
+            this.player.play('walk', 5, true);
+            this.player.scale.x = -1;
         } else if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            // If the RIGHT key is down, set the player velocity to move right
             this.player.body.acceleration.x = this.ACCELERATION;
+            this.player.play('walk', 5, true);
+            this.player.scale.x = 1;
         } else {
             this.player.body.acceleration.x = 0;
+            this.player.frame = 0;
         }
 
-        // Set a variable that is true when the player is touching the ground
+
         var onTheGround = this.player.body.touching.down;
 
         if (onTheGround && this.input.keyboard.justPressed(Phaser.Keyboard.UP)) {
-            // Jump when the player is touching the ground and the up arrow is pressed
+
             this.player.body.velocity.y = this.JUMP_SPEED;
         }
 
